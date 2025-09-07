be1dc5c1899445622ad4f430c2d6c3be4b3ea559
Kuntadhutade <kuntadhutade@gmail.com>
2025-09-07 14:52:18 +0530

    cleaned up Assinmentproskill.html folder

---
+++ aimation/script.js
@@ -0,0 +1,18 @@
+
+
+
+
+    const box = document.getElementById("box");
+
+    function fadeIn(){
+        box.classList.remove("hidden");
+        box.classList.add("visible");
+    }
+
+    function fadeOut(){
+        box.classList.remove("visible");
+        box.classList.add("hidden");
+    }
+
+
+   
